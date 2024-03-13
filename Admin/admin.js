21f4f203c00c461ae70c8edc6f0f06fb991d1be5
ThisIs-Developer <baivabsarkar@gmail.com>
2024-03-14 01:53:23 +0530

    Logout page 3

--- Admin/admin.js
+++ Admin/admin.js
@@ -109,13 +109,8 @@ document.querySelectorAll('.top #close-btn').forEach(button => {
     });
 });
 
-// document.getElementById('logout-link').addEventListener('click', function(event) {
-//     event.preventDefault();
-//     localStorage.removeItem('token');
-//     window.location.href = this.getAttribute('href');
-// });
-
 document.getElementById('logout-link').addEventListener('click', function(event) {
     event.preventDefault();
+    localStorage.removeItem('token');
     window.location.href = this.getAttribute('href');
 });
