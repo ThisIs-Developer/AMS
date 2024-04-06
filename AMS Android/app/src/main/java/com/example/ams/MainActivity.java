ea44da1017899751797763173e9d2c3942a135bc
ThisIs-Developer <baivabsarkar@gmail.com>
2024-04-06 21:00:34 +0530

    Add Android App.
    Co-authored-by: Arpan550 <arpanbhattacharya5356@gmail.com>

---
+++ AMS Android/app/src/main/java/com/example/ams/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.ams;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        webView=findViewById(R.id.webView);
+        webView.loadUrl("https://ams-system.vercel.app/");
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new webViewController());
+    }
+
+}
